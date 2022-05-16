88a365f68ee90738bebada122ccbf72aea3c30f5
EnzoBatista <enzo.batista.u@gmail.com>
2022-05-16 12:05:26 -0300

    httpHook

---
+++ src/hooks/use-http.js
@@ -0,0 +1,39 @@
+import { useState, useCallback } from 'react';
+
+const UseHttp = (requestConfig, applyData) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        requestConfig.url + '?' + new URLSearchParams(requestConfig.params),
+        {
+          method: requestConfig.method ? requestConfig.method : 'GET',
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? requestConfig.body : null,
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('something went wrong');
+      }
+      const data = await response.json();
+      applyData(data);
+
+    } catch (error) {
+        setError(error.message);
+    }
+    setLoading(false);
+  }, [requestConfig, applyData]);
+
+  return {
+      sendRequest,
+      loading,
+      error
+  }
+};
+
+export default UseHttp;
